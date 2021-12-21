f0605d15049e501669b00faa584abdc41dee7316
iammiori <immioriii@gmail.com>
2021-12-21 23:44:12 +0900

    [init] init the project

---
+++ GetGithubReposit/RxSwiftPractice/RepositListCell.swift
@@ -0,0 +1,8 @@
+//
+//  RepositListCell.swift
+//  RxSwiftPractice
+//
+//  Created by miori Lee on 2021/12/21.
+//
+
+import UIKit
---
+++ GetGithubReposit/RxSwiftPractice/RepositListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RepositListViewController.swift
+//  RxSwiftPractice
+//
+//  Created by miori Lee on 2021/12/21.
+//
+
+import UIKit
+
+class RepositListViewController : UITableViewController {
+    
+}

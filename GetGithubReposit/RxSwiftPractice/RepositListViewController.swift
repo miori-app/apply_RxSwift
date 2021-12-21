741d54b5c0a0cd7c523882c401480c6705ddc7d1
iammiori <immioriii@gmail.com>
2021-12-22 04:47:54 +0900

    [feat] 레포짓 테이블뷰 정의

--- GetGithubReposit/RxSwiftPractice/RepositListViewController.swift
+++ GetGithubReposit/RxSwiftPractice/RepositListViewController.swift
@@ -8,5 +8,39 @@
 import UIKit
 
 class RepositListViewController : UITableViewController {
+    private let organization = "miori"
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // navigation title
+        title = organization + "Reposit"
+        
+        // 당겨서 새로고침
+        self.refreshControl = UIRefreshControl()
+        let refreshControl = self.refreshControl!
+        refreshControl.backgroundColor = .white
+        refreshControl.tintColor = .darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "당겨서 새로고침")
+        refreshControl.addTarget(self, action: #selector(refreshTable), for: .valueChanged)
+        
+        tableView.register(RepositListCell.self, forCellReuseIdentifier: "RepositListCell")
+        tableView.rowHeight = 140
+    }
+    
+    @objc func refreshTable() {
+        
+    }
+    
+}
+
+//UITableView DataSource Delegate
+extension RepositListViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RepositListCell", for: indexPath) as? RepositListCell else { return UITableViewCell()}
+        
+        return cell
+    }
 }

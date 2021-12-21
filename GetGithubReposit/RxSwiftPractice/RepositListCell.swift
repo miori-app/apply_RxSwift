cda29f7924b1d76acb9afd1c4e64ba6b8ce1c445
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2021-12-22 04:49:06 +0900

    Set UI  (#1)
    * [feat] UI작업 위한 snapkit SPM으로 관리
    
    * [feat] 레포짓 테이블 뷰 셀 정의
    
    * [feat] 레포짓 테이블뷰 정의

--- GetGithubReposit/RxSwiftPractice/RepositListCell.swift
+++ GetGithubReposit/RxSwiftPractice/RepositListCell.swift
@@ -6,3 +6,25 @@
 //
 
 import UIKit
+import SnapKit
+
+class RepositListCell : UITableViewCell {
+    var reposit : String?
+    
+    let nameLabel = UILabel()
+    let descriptionLabel = UILabel()
+    let startImageView = UIImageView()
+    let starLabel = UILabel()
+    let languageLabel = UILabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //map vs foreach
+        [
+            nameLabel,descriptionLabel,startImageView,starLabel,languageLabel
+        ].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+}
